a176fb10aabf47894fe6d366e41c12430eb9fac5
lollipop <liujian594@hotmail.com>
2020-10-27 01:27:45 +0800

    推进UI布局套件的开发

--- iconKit/src/main/java/com/lollipop/iconkit/dialog/PreviewIconDialog.kt
+++ iconKit/src/main/java/com/lollipop/iconkit/dialog/PreviewIconDialog.kt
@@ -1,13 +1,14 @@
 package com.lollipop.iconkit.dialog
 
+import android.animation.Animator
+import android.animation.ValueAnimator
 import android.app.Activity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewManager
 import com.lollipop.iconcore.listener.BackPressedListener
 import com.lollipop.iconcore.ui.IconImageView
-import com.lollipop.iconcore.util.lifecycleBinding
-import com.lollipop.iconcore.util.onEnd
+import com.lollipop.iconcore.util.log
 import com.lollipop.iconkit.R
 import com.lollipop.iconkit.dialog.base.CurtainDialog
 
@@ -16,7 +17,9 @@ import com.lollipop.iconkit.dialog.base.CurtainDialog
  * @date 10/26/20 23:09
  * 预览Icon的Dialog
  */
-class PreviewIconDialog: BackPressedListener {
+class PreviewIconDialog: BackPressedListener,
+    ValueAnimator.AnimatorUpdateListener,
+    Animator.AnimatorListener {
 
     private var dialogView: View? = null
 
@@ -36,18 +39,23 @@ class PreviewIconDialog: BackPressedListener {
             return dialogView?.findViewById(R.id.iconView)
         }
 
+    private var progress = 0F
+
+    private val animator = ValueAnimator().apply {
+        addUpdateListener(this@PreviewIconDialog)
+        addListener(this@PreviewIconDialog)
+    }
+
     fun attach(context: Activity) {
         remove()
         val rootGroup = CurtainDialog.findGroup(context.window.decorView)
         val view = LayoutInflater.from(context).inflate(
             R.layout.kit_preview_dialog, rootGroup, false)
         dialogView = view
-
-        view.visibility = View.INVISIBLE
-
         view.setOnClickListener {
             dismiss()
         }
+        view.visibility = View.INVISIBLE
         rootGroup?.addView(view)
     }
 
@@ -56,8 +64,7 @@ class PreviewIconDialog: BackPressedListener {
     }
 
     private fun remove() {
-        backgroundView?.animate()?.cancel()
-        previewView?.animate()?.cancel()
+        animator.cancel()
         targetView?.visibility = View.VISIBLE
         targetView = null
         dialogView?.let { dialog ->
@@ -71,44 +78,32 @@ class PreviewIconDialog: BackPressedListener {
     }
 
     fun show(view: View, icon: Int) {
-        val dialog = dialogView?:return
         val preview = previewView?:return
-        val background = backgroundView?:return
+        targetView = view
 
-        background.alpha = 0F
-        background.animate().apply {
-            cancel()
-            alpha(1F)
-            start()
+        progress = 0F
+        preview.apply {
+            scaleX = 1F
+            scaleY = 1F
+            translationX = 0F
+            translationY = 0F
         }
+        preview.loadIcon(icon)
+        initInfo(view, preview)
+        view.visibility = View.INVISIBLE
+        doAnimation(true)
+    }
 
+    private fun initInfo(view: View, preview: View) {
         sizeOffset[0] = view.width * 1F / preview.width
         sizeOffset[1] = view.height * 1F / preview.height
 
-        getLocationInRoot(preview, view, locationOffset)
-
-        preview.apply {
-            scaleX = sizeOffset[0]
-            scaleY = sizeOffset[1]
-            translationX = locationOffset[0].toFloat()
-            translationY = locationOffset[1].toFloat()
-            loadIcon(icon)
-        }
-        preview.animate().apply {
-            cancel()
-            scaleX(1F)
-            scaleY(1F)
-            translationX(0F)
-            translationY(0F)
-            start()
-        }
-
-//        view.visibility = View.INVISIBLE
-
-        dialog.visibility = View.VISIBLE
+        locationOffset[0] = 0
+        locationOffset[1] = 0
+        getLocationOffset(preview, view, locationOffset)
     }
 
-    private fun getLocationInRoot(self: View, target: View, intArray: IntArray) {
+    private fun getLocationOffset(self: View, target: View, intArray: IntArray) {
         val selfLoc = IntArray(2)
         self.getLocationInWindow(selfLoc)
         selfLoc[0] -= self.translationX.toInt()
@@ -123,34 +118,19 @@ class PreviewIconDialog: BackPressedListener {
         targetLoc[1] += target.height / 2
         intArray[0] = targetLoc[0] - selfLoc[0]
         intArray[1] = targetLoc[1] - selfLoc[1]
+//        log("[${selfLoc[0]}, ${selfLoc[1]}]  [${targetLoc[0]}, ${targetLoc[1]}]")
+//        log("[${selfLoc[0]}, ${selfLoc[1]}] ${self.width} * ${self.height}  ${self.translationX} * ${self.translationY} ")
     }
 
     private fun dismiss() {
-        val preview = previewView?:return
-        val background = backgroundView?:return
+        doAnimation(false)
+    }
 
-        background.animate().apply {
-            cancel()
-            alpha(0F)
-            start()
-        }
-
-        preview.animate().apply {
-            cancel()
-            scaleX(sizeOffset[0])
-            scaleY(sizeOffset[1])
-            translationX(locationOffset[0].toFloat())
-            translationY(locationOffset[1].toFloat())
-            lifecycleBinding {
-                onEnd {
-                    removeThis(it)
-                    targetView?.visibility = View.VISIBLE
-                    targetView = null
-                    dialogView?.visibility = View.INVISIBLE
-                }
-            }
-            start()
-        }
+    private fun doAnimation(isShow: Boolean) {
+        val endValue = if (isShow) { 1F } else { 0F }
+        animator.cancel()
+        animator.setFloatValues(progress, endValue)
+        animator.start()
     }
 
     override fun onBackPressed(): Boolean {
@@ -159,6 +139,46 @@ class PreviewIconDialog: BackPressedListener {
             return true
         }
         return false
+    }
+
+    private fun onUpdate() {
+        val preview = previewView?:return
+        val background = backgroundView?:return
+        preview.apply {
+            val x = (1 - sizeOffset[0]) * progress + sizeOffset[0]
+            val y = (1 - sizeOffset[1]) * progress + sizeOffset[1]
+            scaleX = x
+            scaleY = y
+            translationX = locationOffset[0] * (1 - progress)
+            translationY = locationOffset[1] * (1 - progress)
+        }
+        background.alpha = progress
+    }
+
+    override fun onAnimationUpdate(animation: ValueAnimator?) {
+        if (animation == animator) {
+            progress = animation.animatedValue as Float
+            onUpdate()
+        }
+    }
+
+    override fun onAnimationStart(animation: Animator?) {
+        if (animation == animator && dialogView?.visibility != View.VISIBLE) {
+            dialogView?.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onAnimationEnd(animation: Animator?) {
+        if (animation == animator && progress < 0.1F) {
+            dialogView?.visibility = View.INVISIBLE
+            targetView?.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onAnimationCancel(animation: Animator?) {
+    }
+
+    override fun onAnimationRepeat(animation: Animator?) {
     }
 
 }
