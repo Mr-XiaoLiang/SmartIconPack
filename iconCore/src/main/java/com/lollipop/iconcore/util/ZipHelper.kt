160ecb159040e16a67ba98ebf69de78d7bf313fb
lollipop <liujian594@hotmail.com>
2020-10-31 00:05:55 +0800

    修复放置重复文件产生的崩溃导致的压缩中断问题

--- iconCore/src/main/java/com/lollipop/iconcore/util/ZipHelper.kt
+++ iconCore/src/main/java/com/lollipop/iconcore/util/ZipHelper.kt
@@ -130,31 +130,35 @@ class ZipHelper private constructor (private var zipFile: File) {
             val buffer = ByteArray(2048)
             while (fileList.isNotEmpty()) {
                 val fileEntry = fileList.removeFirst()
-                fileOutput.putNextEntry(ZipEntry(fileEntry.name))
-                if (fileEntry.isExist) {
-                    if (fileEntry.isDir) {
-                        val listFiles = fileEntry.listFiles()
-                        for (child in listFiles) {
-                            fileList.addLast(FileEntry.create(child, fileEntry))
-                        }
-                    } else {
-                        var inputStream: FileInputStream? = null
-                        try {
-                            inputStream = FileInputStream(fileEntry.file)
-                            var length = inputStream.read(buffer)
-                            while (length >= 0) {
-                                fileOutput.write(buffer, 0, length)
-                                length = inputStream.read(buffer)
+                try {
+                    fileOutput.putNextEntry(ZipEntry(fileEntry.name))
+                    if (fileEntry.isExist) {
+                        if (fileEntry.isDir) {
+                            val listFiles = fileEntry.listFiles()
+                            for (child in listFiles) {
+                                fileList.addLast(FileEntry.create(child, fileEntry))
                             }
-                            fileOutput.flush()
-                        } catch (ee: Throwable) {
-                            ee.printStackTrace()
-                        } finally {
-                            inputStream?.close()
+                        } else {
+                            var inputStream: FileInputStream? = null
+                            try {
+                                inputStream = FileInputStream(fileEntry.file)
+                                var length = inputStream.read(buffer)
+                                while (length >= 0) {
+                                    fileOutput.write(buffer, 0, length)
+                                    length = inputStream.read(buffer)
+                                }
+                                fileOutput.flush()
+                            } catch (ee: Throwable) {
+                                ee.printStackTrace()
+                            } finally {
+                                inputStream?.close()
+                            }
                         }
                     }
+                    fileOutput.closeEntry()
+                } catch (ee: Throwable) {
+                    ee.printStackTrace()
                 }
-                fileOutput.closeEntry()
             }
             fileOutput.flush()
         } catch (e: Throwable) {
