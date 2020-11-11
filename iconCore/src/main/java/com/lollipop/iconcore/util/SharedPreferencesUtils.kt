bcb511c2d77b345ba5505921a955be8601d8b15a
lollipop <liujian594@hotmail.com>
2020-11-11 17:52:06 +0800

    移除不必要的代码

--- iconCore/src/main/java/com/lollipop/iconcore/util/SharedPreferencesUtils.kt
+++ iconCore/src/main/java/com/lollipop/iconcore/util/SharedPreferencesUtils.kt
@@ -55,17 +55,15 @@ object SharedPreferencesUtils {
      * @param value 值，目前只支持String，Long，Boolean，Int，Float
      */
     fun <T> put(mShareConfig: SharedPreferences, key: String, value: T) {
-        if (notNull(value)) {
-            val conEdit = mShareConfig.edit()
-            when (value) {
-                is String -> conEdit.putString(key, (value as String).trim { it <= ' ' })
-                is Long -> conEdit.putLong(key, value as Long)
-                is Boolean -> conEdit.putBoolean(key, value as Boolean)
-                is Int -> conEdit.putInt(key, value as Int)
-                is Float -> conEdit.putFloat(key, value as Float)
-            }
-            conEdit.apply()
+        val conEdit = mShareConfig.edit()
+        when (value) {
+            is String -> conEdit.putString(key, (value as String).trim { it <= ' ' })
+            is Long -> conEdit.putLong(key, value as Long)
+            is Boolean -> conEdit.putBoolean(key, value as Boolean)
+            is Int -> conEdit.putInt(key, value as Int)
+            is Float -> conEdit.putFloat(key, value as Float)
         }
+        conEdit.apply()
     }
 
     /**
@@ -122,23 +120,14 @@ object SharedPreferencesUtils {
     @Suppress("UNCHECKED_CAST")
     fun <T> get(mShareConfig: SharedPreferences, key: String, defValue: T): T {
         var value: T = defValue
-        if (notNull(key)) {
-            when (defValue) {
-                is String -> value = mShareConfig.getString(key, defValue as String) as T
-                is Long -> value = java.lang.Long.valueOf(mShareConfig.getLong(key, defValue as Long)) as T
-                is Boolean -> value = java.lang.Boolean.valueOf(mShareConfig.getBoolean(key, defValue as Boolean)) as T
-                is Int -> value = Integer.valueOf(mShareConfig.getInt(key, defValue as Int)) as T
-                is Float -> value = java.lang.Float.valueOf(mShareConfig.getFloat(key, defValue as Float)) as T
-            }
+        when (defValue) {
+            is String -> value = mShareConfig.getString(key, defValue as String) as T
+            is Long -> value = java.lang.Long.valueOf(mShareConfig.getLong(key, defValue as Long)) as T
+            is Boolean -> value = java.lang.Boolean.valueOf(mShareConfig.getBoolean(key, defValue as Boolean)) as T
+            is Int -> value = Integer.valueOf(mShareConfig.getInt(key, defValue as Int)) as T
+            is Float -> value = java.lang.Float.valueOf(mShareConfig.getFloat(key, defValue as Float)) as T
         }
         return value
-    }
-
-    /**
-     * object not null
-     */
-    private fun notNull(obj: Any?): Boolean {
-        return null != obj
     }
 
 }
